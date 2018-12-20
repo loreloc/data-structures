17d15401e2f532c5f73db5b68444d7dc22e81adc
loreloc <lorenzoloconte@outlook.it>
2018-12-20 19:53:01 +0100

    added subtree remove in binarytree.cpp

--- tests/binarytree.cpp
+++ tests/binarytree.cpp
@@ -4,14 +4,18 @@
 int main(int argc, char *argv[])
 {
 	BinaryTree<int> tree;
-	BTNode<int> *p;
 
-	p = tree.insertRoot(0);
-	tree.insertLeft(p, 1);
-	p = tree.insertRight(p, 2);
-	tree.insertLeft(p, 3);
-	p = tree.insertRight(p, 4);
-	tree.insertRight(p, 5);
+	BTNode<int> *p0 = tree.insertRoot(0);
+
+	BTNode<int> *p1 = tree.insertLeft(p0, 1);
+	BTNode<int> *p2 = tree.insertRight(p0, 2);
+	BTNode<int> *p3 = tree.insertLeft(p2, 3);
+	BTNode<int> *p4 = tree.insertRight(p2, 4);
+	BTNode<int> *p5 = tree.insertRight(p4, 5);
+
+	std::cout << tree << std::endl;
+
+	tree.removeRight(p2);
 
 	std::cout << tree << std::endl;
 
